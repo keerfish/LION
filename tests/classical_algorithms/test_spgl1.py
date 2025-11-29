ff443aeb49279041e6ca3f78aa7bea3407f62e03
ttv22 <ttv22@cam.ac.uk>
2025-11-29 19:55:54 +0000

    Fix unit tests

--- tests/classical_algorithms/test_spgl1.py
+++ tests/classical_algorithms/test_spgl1.py
@@ -1,5 +1,29 @@
 """Tests for SPGL1."""
 
+import torch
+from LION.classical_algorithms.spgl1 import spgl1_torch
+from LION.operators import CompositeOp, PhotocurrentMapOp, Subsampler, Wavelet2D
 
-def test_spgl1():
-    raise NotImplementedError("Please implement tests for SPGL1.")
+
+def test_spgl1() -> None:
+    device = "cpu"
+    # Image size
+    J = 4  # 16x16 images
+    H = W = 1 << J
+    coarseJ = J - 1
+    delta = 1.0 / 4
+
+    # Wavelet transform Psi
+    wavelet = Wavelet2D((H, W), wavelet_name="db4", device=device)
+
+    # Photocurrent mapping operator Phi
+    subsampler = Subsampler(n=H * W, coarseJ=coarseJ, delta=delta)
+    phi = PhotocurrentMapOp(J=J, subsampler=subsampler, device=device)
+
+    # Composite operator A = Phi Psi^{-1}
+    A_op = CompositeOp(wavelet, phi, device=device)
+
+    x = torch.rand(A_op.domain_shape, device=device)
+    y = phi(x)
+
+    assert spgl1_torch(op=A_op, y=y, iter_lim=1) is not None
--- tests/operators/test_ct_op.py
+++ tests/operators/test_ct_op.py
@@ -1,9 +1,10 @@
-"""Tests for CT operator wrapped with tomosipo's to_autograd function."""
+"""Tests for CT operator."""
 
 import pytest
 import torch
 from LION.CTtools.ct_geometry import Geometry
 from LION.CTtools.ct_utils import make_operator
+from tests.helper import dotproduct_adjointness_test
 from tomosipo.torch_support import to_autograd
 
 
@@ -67,3 +68,47 @@ def test_original_op_does_not_propagate_grad():
         RuntimeError, match="does not require grad and does not have a grad_fn"
     ):
         output_tensor.mean().backward()
+
+
+def test_ct_op_adjointness():
+    """Test CT operator adjoint property."""
+    geometry = Geometry.default_parameters()
+    operator = make_operator(geometry=geometry)
+
+    # Check the default operator shapes
+    assert operator.domain_shape == (1, 512, 512)
+    assert operator.range_shape == (1, 360, 900)
+
+    # Create a test input for the forward and backward projections
+    test_volume = torch.rand(*operator.domain_shape)
+    test_projection = torch.rand(*operator.range_shape)
+    # Note: tomosipo implementation uses an accelerated method that is not
+    # exactly adjoint, so we use looser tolerances here.
+    dotproduct_adjointness_test(
+        operator,
+        test_volume,
+        test_projection,
+        relative_tolerance=1e-1,
+        absolute_tolerance=1e-1,
+    )
+
+
+def test_ct_op_backward_compatibility_with_tomosipo():
+    geometry = Geometry.default_parameters()
+    operator = make_operator(geometry=geometry)
+
+    # Check the default operator shapes
+    assert operator.domain_shape == (1, 512, 512)
+    assert operator.range_shape == (1, 360, 900)
+
+    # Create a test input for the forward and backward projections
+    test_volume = torch.rand(*operator.domain_shape)
+    test_projection = torch.rand(*operator.range_shape)
+
+    # Make sure that tomosipo Operator's attributes are accessible
+    assert operator._fp(volume=test_volume, out=None) is not None
+    assert operator._bp(projection=test_projection, out=None) is not None
+    assert operator.transpose() is not None
+    assert operator.T is not None
+    assert operator.domain is not None
+    assert operator.range is not None
